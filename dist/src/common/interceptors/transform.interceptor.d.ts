834a80c9b7b105744dce9b7b20c941dd4ceb0c71
wuxudan <wuxudan@nefu.edu.cn>
2026-05-14 14:51:37 +0800

    chore: update gitignore and stop tracking build artifacts

--- dist/src/common/interceptors/transform.interceptor.d.ts
+++
@@ -1,9 +0,0 @@
-import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { Observable } from 'rxjs';
-export declare class TransformInterceptor implements NestInterceptor {
-    intercept<T>(context: ExecutionContext, next: CallHandler<T>): Observable<{
-        data: T;
-        code: number;
-        message: string;
-    }>;
-}
--- dist/src/modules/tenant/tenant.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class TenantService {
-    private tenantId;
-    setTenantId(id: string): void;
-    getTenantId(): string;
-}
